eb1804bad465679bc525e2143149ade5124b79db
J-AMD <abrinez1@gmail.com>
2020-02-03 15:23:50 -0600

    My first commit(encriptio only 1 case)

---
+++ Encription.py
@@ -0,0 +1,20 @@
+import key
+
+#entry = open('input.txt', 'r')
+#inbox = entry.read()
+#entry.close()
+#print (inbox)
+salida = ""
+
+inbox = "cx kuyxnkfu úrj hcxujxqx hxlx jc qbx qj cx pdáx qj odókjlud ju hrjócx jc yjcdü árlskacxod mkuqt sdábx újckü sxlqkccd z ikík srxuqd cx skovjex pdsxóx jc nxñdúfu qjplwn qjc hxcjuérj qj hxgx qj cdn wuojcjn sdu xrpdlküxskfu qjc áxlknsxc qjc jgjlskpd qj cx uxskfu sdu jc úku qj ljsrhjlxl cx hcxüx qj cdn ukedn"
+
+for letter in range(0,(len(inbox))):
+	letra = inbox[letter]
+	if chr(32) != letra:
+		salida += (key.cifrado[letra])
+	else:
+		salida += (" ")
+
+print (salida)
+	
+
---
+++ key.py
@@ -0,0 +1,38 @@
+#Diccionario con la clave de cifrado para cada elemento o caracter
+
+cifrado = { 'x': "a",
+			'ó': "b",
+			's': "c",
+			'q': "d",
+			'j': "e",
+			'ú': "f",
+			'o': "g",
+			'm': "h",
+			'k': "i",
+			'g': "j",
+			'i': "k",
+			'c': "l",
+			'á': "m",
+			'u': "n",
+			'e': "ñ",
+			'd': "o",
+			'h': "p",
+			'é': "q",
+			'l': "r",
+			'n': "s",
+			'p': "t",
+			'r': "u",
+			'y': "v",
+			'í': "w",
+			'ñ': "x",
+			'z': "y",
+			'ü': "z",
+			'w': "á",	#Vocales con asento y dieresis
+			'a': "é",
+			'b': "í",
+			'f': "ó",
+			't': "ú",
+			'v': "ü"
+			#'Ã': "f",
+			#'º': "u"
+}
